be9e5925baf96e09f838ee813d7832837af1eb40
jjub0217 <noonssub1004@gmail.com>
2021-02-14 17:52:45 +0900

    feat: content page

--- js/content.js
+++ js/content.js
@@ -1,3 +1,27 @@
+import {
+    loadedLocalStorage
+} from './export.js';
 
-const  loadDairy = require('./detail.js');
-console.log(loadDairy());
+
+const $contentTime = document.querySelector('.contents-time_feeling');
+
+
+
+function callDatas() {
+    const datas = loadedLocalStorage();
+
+    let titleHtml = '';
+
+    datas.forEach(data => {
+
+        titleHtml += `
+        <span class="day contents-day">${data.dayString}</span>
+        <span class="month contents-Month">${data.month < 10 ? `0${data.month}` : data.month}</span>
+        <span class="date contents-date">${data.date}</span>
+        <span class="year contents-year">${data.year}</span>
+        <img src="./images/feeling/good-regular.png" alt="feeling" class="icon contents-icon" />`
+    })
+
+    $contentTime.innerHTML = titleHtml;
+}
+callDatas();
