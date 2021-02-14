21ba2e9aafdf05174a7ef8ec4556fcdcfe4376b8
jjub0217 <noonssub1004@gmail.com>
2021-02-14 14:38:23 +0900

    fix: ul > li content rendering using template literals and localstorage getItem mehod and Date built-in function

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,97 @@
-// const year = document.querySelector('.year');
-// const month = document.querySelector('.year');
-// const date = document.querySelector('.year');
+const $h2 = document.querySelector('.list-title');
+const $ul = document.querySelector('.board-list');
+const $time = document.querySelector('.time');
+
+
+function loadHeaderDate() {
+    // let html = '';
+    const loadedHeaderDate = new Date();
+    console.log(loadedHeaderDate);
+    const year = loadedHeaderDate.getFullYear();
+    const month = loadedHeaderDate.getMonth();
+    const date = loadedHeaderDate.getDate();
+
+    if(month <10){
+        $time.innerHTML = `${year} 0${month} ${date}`;
+    }else{
+        $time.innerHTML = `${year} ${month} ${date}`;
+    }
+}
+
+loadHeaderDate();
+
+
+
+
+function loadDairy() {
+    const loadedDairy = localStorage.getItem('dairy');
+    console.log(loadedDairy);
+
+    if (loadedDairy !== null) {
+        // 로컬스토리지에 저장된 데이터
+        const parsedDairy = JSON.parse(loadedDairy);
+        console.log(parsedDairy);
+
+        let html = '';
+
+        parsedDairy.forEach(dairy => {
+
+            const title = dairy.title;
+            const tags = dairy.tags;
+            const year = dairy.year;
+            const month = dairy.month;
+            const date = dairy.date;
+            const day = dairy.dayString;
+            const hour = dairy.hour;
+            // console.log(hour);
+            const minute = dairy.minute;
+            // console.log(minute);
+
+            let tagHtml = '';
+
+            // console.log(tags); // [# 힘들어, #씻구와]
+            tags.forEach(tag => {
+                tagHtml += ` 
+                <span>${tag}</span>`
+            })
+            console.log(tagHtml);
+
+
+            html += `
+            <li class="list">
+          <a href="./contents.html">
+
+            <div class="list-date">
+              <span class="day">${day}</span>
+              <span class="date">${date}</span>
+            </div>
+
+
+            <div class="contents">
+
+              <div class="text">
+                <div class="detail-time">
+                  <span class="detail-date">${hour}:${minute}</span>
+                </div>
+
+                <h2 class="list-title">${title}</h2>
+
+                <div class="tag">
+                ${tagHtml}                  
+                </div>
+              </div>
+
+              <img src="./images/feeling/good-regular.png" alt="feeling" class="icon feeling-icon" />
+
+            </div>
+
+          </a>
+        </li>
+            `
+        });
+
+        $ul.innerHTML = html;
+    }
+}
+
+loadDairy();
